49883727c9914e429068c71cbba1dcf821dc84a8
ericbaldwin <ebaldwin@linkedin.com>
2023-08-03 21:23:34 -0700

    updated bet amount to lose 2x

--- src/functions.js
+++ src/functions.js
@@ -4,34 +4,35 @@ function rollDice(percentage) {
 }
 
 function chanceToWin(startingAmt, betamount, numberofbets, percentage) {
-    let totalLosses = 0;
     for (let x = 0; x < numberofbets; x++) {
+        // console.log('current bet amount', startingAmt)
         if (startingAmt >= betamount) {
             if (rollDice(percentage)) {
                 startingAmt += betamount * 2;
             } else {
-                startingAmt -= betamount;
-                totalLosses += 1;
+                startingAmt -= betamount * 2;
             }
         }
     }
-    return { currentbet: startingAmt, totalLosses: totalLosses };
+    return startingAmt
 }
 
 export function total(totalRuns, startingAmt, betAmount, numberOfBets, percentage) {
-    let numRuns = 0;
     let avg = 0;
     let total = 0;
     let totalLosses = 0;
     for (let x = 0; x < Number(totalRuns); x++) {
-        numRuns += 1;
-        // console.log(`current bet # ${numRuns}`);
-        const {currentbet, totalLosses: losses} = chanceToWin(Number(startingAmt), Number(betAmount), Number(numberOfBets), Number(percentage));
-        // console.log(`current bet leftover ${currentbet}`);
-        total += currentbet;
-        // console.log(`total amount ${total}`);
-        totalLosses += losses
+        let currentbet = chanceToWin(Number(startingAmt), Number(betAmount), Number(numberOfBets), Number(percentage));
+        // console.log('ending amount ', currentbet)
+        if (currentbet === 0) {
+            totalLosses += 1
+        } else {
+            total += currentbet
+        }
     }
-    avg = total / numRuns;
-    return `average return: $${avg} total amount after bets: $${total} total losses: $${totalLosses}`;
+    let totalAvg = total / totalRuns;
+    avg = total / (totalRuns - totalLosses);
+    return `average return of non 0 runs: $${avg.toFixed(2)}
+    average return of all total runs: $${totalAvg.toFixed(2)}
+    Number of times it hit 0: ${totalLosses} times`;
 }
